3a9cf2e162eabdc672175fa573d926100866517e
wikixen <bennylopezhernandez0@gmail.com>
2025-08-04 22:32:17 -0400

    DID INPUT; DIDNT WORK BECAUSE MY BUTTON WAS THE WRONG WAY :smiling_face_with_tear:

--- switch/switch.c
+++ switch/switch.c
@@ -1,25 +1,24 @@
 // switch.c turns on led via button, both of which are on a breadboard
 
-#include <avr/io.h>
 #include <util/delay.h>
-// #include </home/bennyhernandez/Code/arduino_avr/myports.h>
+#include </home/bennyhernandez/Code/arduino_avr/myports.h>
 
 int main(void) {
-  DDRD |= (1 << DDD3);
+  DDRD |= (1 << 3); // LED Output
 
-  DDRD &= ~(1 << DDD2);
-
-  PORTD |= (1 << PORTD2);
+  // Btn input
+  DDRD &= ~(1 << 2); 
+  PORTD |= (1 << 2);
 
   while (1)
   {
-    if (~(PIND & (1<<PIND2)))
+    if (~PIND & (1<<2))
     {
-      PORTD |= (1 << PORTD3);
+      PORTD |= (1 << 3);
     }
-    else
+    else 
     {
-      PORTD &= ~(1 << PORTD3);
+      PORTD &= ~(1 << 3);
     }
   }
 }
