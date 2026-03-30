4df0299b2a0e0016b413bda075dd2f6f4aae5a9e
wikixen <bennylopezhernandez0@gmail.com>
2026-03-29 20:40:03 -0400

    added the ADC set up to loveometer.c, still need to add logic in while loop

--- src/loveometer.c
+++ src/loveometer.c
@@ -5,16 +5,24 @@
  */
 #include <avr/io.h>
 
-void analogInit() {
-  ADMUX |= (1 << REFS0) | (0 << MUX0); // Ensures Pin A0 is input
-  ADCSRA = 0b10000011; // Sets precaler to 1MHz
+void initADC();
+
+void initADC()
+{
+  ADMUX |= (1 << REFS0) | (0 << MUX0); // Sets VCC to on & ensures Pin A0 is input
+  ADCSRA |= (1 << ADEN) | (1 << ADPS1) | (1 << ADPS0); // ADEN sets ADC to on & ADPS sets precaler to 1MHz
 }
 
 int main() {
-  analogInit();
+  uint16_t ADCValue = 0;
+  DDRD |= (1 << PD2) | (1 << PD3) | (1 << PD4); // Sets Pins D2,3,4 as outputs
+  PORTC = 0; // Make sure that C0 is an input 
+  initADC();
 
   while (1)
   {
+    ADCSRA |= (1 << ADSC);
+    loop_until_bit_is_clear(ADCSRA, ADSC);
     /* code */
   }
   
